b04c3422ba6e2b8c9521fe8b4b01b1e515d48fdc
saraiva142 <saraivajandrade@gmail.com>
2025-06-25 16:47:26 -0300

    feat: Método Guest com /users ok 🦒

--- src/main/java/com/picpaysimplificado/domain/user/User.java
+++ src/main/java/com/picpaysimplificado/domain/user/User.java
@@ -2,10 +2,7 @@ package com.picpaysimplificado.domain.user;
 
 import com.picpaysimplificado.dtos.UserDTO;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -14,6 +11,7 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class User {
 
