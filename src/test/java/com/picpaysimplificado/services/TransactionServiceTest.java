a23dfd25ba9e72599493e6bf4e44e19b116cf847
saraiva142 <saraivajandrade@gmail.com>
2025-06-26 15:04:38 -0300

    feat: Teste-> Should throw exception when transaction is not allowed 🐓

--- src/test/java/com/picpaysimplificado/services/TransactionServiceTest.java
+++ src/test/java/com/picpaysimplificado/services/TransactionServiceTest.java
@@ -4,6 +4,7 @@ import com.picpaysimplificado.domain.user.User;
 import com.picpaysimplificado.domain.user.UserType;
 import com.picpaysimplificado.dtos.TransactionDTO;
 import com.picpaysimplificado.repositories.TransactionRepositry;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -74,7 +75,24 @@ class TransactionServiceTest {
 
     @Test
     @DisplayName("Should throw exception when transaction is not allowed")
-    void createTransactionIsNotAllowed() {
+    void createTransactionIsNotAllowed() throws Exception {
+        //Arrange
+        User sender = new User(1L, "Maria", "Souza", "99999999901", "maria@email.com", "12345", new BigDecimal(10), UserType.COMMON);
+        User receiver = new User(2L, "Joao", "Souza", "99999999902", "joao@email.com", "12345", new BigDecimal(10), UserType.COMMON);
+
+        when(userService.findUserById(1L)).thenReturn(sender);
+        when(userService.findUserById(2L)).thenReturn(receiver);
+
+        when(authService.authorizeTransaction(any(), any())).thenReturn(false);
+
+        Exception thrown = Assertions.assertThrows(Exception.class, () -> {
+            TransactionDTO request = new TransactionDTO(new BigDecimal(10), 1L, 2L);
+            //Act
+            transactionService.createTransaction(request);
+        });
+
+        //Assert
+        Assertions.assertEquals("Transação não autorizada", thrown.getMessage());
 
     }
 }
