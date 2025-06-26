bb7450a38e96ac33a2c953814fc23779bfd29810
saraiva142 <saraivajandrade@gmail.com>
2025-06-26 14:09:07 -0300

    feat: Teste-> Should get user successfully from DB 🐊

--- src/test/java/com/picpaysimplificado/repositories/UserRepositoryTest.java
+++ src/test/java/com/picpaysimplificado/repositories/UserRepositoryTest.java
@@ -1,10 +1,19 @@
 package com.picpaysimplificado.repositories;
 
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.dtos.UserDTO;
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.context.annotation.ApplicationScope;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,8 +21,32 @@ import static org.junit.jupiter.api.Assertions.*;
 @ActiveProfiles("test")
 class UserRepositoryTest {
 
-    @Test
-    void findUserByDocument() {
+    @Autowired
+    UserRepository userRepository;
 
+    @Autowired
+    EntityManager entityManager;
+
+    @Test
+    @DisplayName("Should get user successfully from DB")
+    void findUserByDocumentSucces() {
+
+        //Arrange
+        String document = "99999999901";
+        UserDTO data = new UserDTO("Fernanda", "Teste", document, new BigDecimal(10), "test@email.com", "44444", UserType.COMMON);
+        this.createUser(data);
+
+        //Act
+        Optional<User> result = this.userRepository.findUserByDocument(document);
+
+        //Assert
+        assertThat(result.isPresent()).isTrue();
+
+    }
+
+    private User createUser(UserDTO data) {
+        User newUser = new User(data);
+        this.entityManager.persist(newUser);
+        return newUser;
     }
 }
