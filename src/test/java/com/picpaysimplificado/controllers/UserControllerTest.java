e1a06da3fc711c7e398efdac4843da27274f0766
saraiva142 <saraivajandrade@gmail.com>
2025-07-07 15:47:28 -0300

    feat: Teste-> Should create a user successfully and return 200 is OK 💸

---
+++ src/test/java/com/picpaysimplificado/controllers/UserControllerTest.java
@@ -0,0 +1,94 @@
+package com.picpaysimplificado.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.dtos.UserDTO;
+import com.picpaysimplificado.services.UserService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserController.class)
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private UserService userService;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Nested
+    class createUsers {
+
+        @Test
+        @DisplayName("Should create a user successfully and return 200")
+        void createUserWithSuccess() throws Exception {
+            //Arrange
+
+            UserDTO request = new UserDTO(
+                    "First Name",
+                    "Last Name",
+                    "123456789",
+                    new BigDecimal(100),
+                    "email@email.com",
+                    "pass123",
+                    UserType.COMMON
+            );
+
+            Long id = 1L;
+            User user = new User();
+            user.setId(id);
+            user.setFirstname("First Name");
+            user.setLastname("Last Name");
+            user.setEmail("email@email.com");
+            user.setBalance(new BigDecimal(200));
+            user.setDocument("123456789");
+            user.setUserType(UserType.COMMON);
+
+            User exceptedUser = new User();
+            exceptedUser.setId(id);
+            exceptedUser.setFirstname("First Name");
+            exceptedUser.setLastname("Last Name");
+            exceptedUser.setEmail("email@email.com");
+            exceptedUser.setBalance(new BigDecimal(200));
+            exceptedUser.setDocument("123456789");
+            exceptedUser.setUserType(UserType.COMMON);
+
+            when(userService.createUser(any(UserDTO.class))).thenReturn(exceptedUser);
+
+            //Act & Assert
+            mockMvc.perform(post("/users")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(request)))
+                    .andExpect(status().isCreated())
+                    .andExpect(jsonPath("$.id").value(exceptedUser.getId()))
+                    .andExpect(jsonPath("$.document").value(exceptedUser.getDocument()));
+
+            verify(userService, times(1)).createUser(any(UserDTO.class));
+        }
+
+    }
+
+
+    @Test
+    void getAllUsers() {
+    }
+}
