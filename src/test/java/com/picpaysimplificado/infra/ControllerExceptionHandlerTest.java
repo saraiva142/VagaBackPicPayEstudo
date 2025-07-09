a6fab29148db65298f46bb012044da51699e11b7
saraiva142 <saraivajandrade@gmail.com>
2025-07-09 13:50:11 -0300

    feat: Teste-> Should return 404 not found with empty body is OK  FIM 🚀

--- src/test/java/com/picpaysimplificado/infra/ControllerExceptionHandlerTest.java
+++ src/test/java/com/picpaysimplificado/infra/ControllerExceptionHandlerTest.java
@@ -1,16 +1,47 @@
 package com.picpaysimplificado.infra;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@WebMvcTest(controllers = ControllerExceptionHandlerTest.class)
 class ControllerExceptionHandlerTest {
+
+    private ControllerExceptionHandler handler = new ControllerExceptionHandler();
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
 
     @Test
     void threatDuplicateEntry() {
     }
 
     @Test
+    @DisplayName("Should return 404 not found with empty body")
     void threat404() {
+        //Arrange
+        EntityNotFoundException exception = new EntityNotFoundException("Usuário não encontrado para o ID fornecido");
+
+        //Act
+        ResponseEntity response = handler.threat404(exception);
+
+        //Assert
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertNull(response.getBody());
+        assertTrue(response.hasBody() == false);
+
+
     }
 }
